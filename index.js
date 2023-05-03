7184170897683600ca912c9e59eb8915d6055ba3
Kamrul101 <ridoyhasan87@gmail.com>
2023-05-04 03:09:17 +0600

    server for single recipe

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 const chef = require("./Data/chef.json");
 const recipe = require("./Data/recipe.json");
+const recipeSingle = require("./Data/recipe.json");
 app.use(cors());
 app.get("/", (req, res) => {
   res.send("Chef is running");
@@ -15,6 +16,9 @@ app.get("/chef", (req, res) => {
 app.get("/recipe", (req, res) => {
   res.send(recipe);
 });
+app.get("/recipeSingle", (req, res) => {
+  res.send(recipeSingle);
+});
 app.get("/chef/:id", (req, res) => {
   const id = req.params.id;
   const selectedChef = chef.find((n) => n.id === id);
@@ -23,6 +27,7 @@ app.get("/chef/:id", (req, res) => {
 
 app.get("/recipe/:id", (req, res) => {
   const id = parseInt(req.params.id);
+  // console.log(id);
   if (id == 0) {
     res.send(recipe);
   } else {
@@ -30,6 +35,12 @@ app.get("/recipe/:id", (req, res) => {
     res.send(recipeList);
   }
 });
+app.get("/recipeSingle/:id", (req, res) => {
+  const id = req.params.id;
+  const recipeDetails = recipeSingle.find((n) => n.r_Id === id);
+  res.send(recipeDetails);
+});
+
 
 app.listen(port, () => {
   console.log(`Chef API is running on port: ${port}`);
